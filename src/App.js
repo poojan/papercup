858635a8464864003ffc52af6978dd472e397443
Poojan Shrestha <poojans@gmail.com>
2016-12-19 08:24:41 +0545

    Made three js container reactive.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import { observer } from 'mobx-react';
 @observer
 class App extends Component {
   componentDidMount() {
+    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
     this.renderImage(this.props.store);
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.renderImage(nextProps.store);
+  componentWillReact() {
+    this.renderImage(this.props.store);
   }
 
   renderImage(store) {
-    var renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(store.bg.width, store.bg.height);
-    renderer.setClearColor( 0x000000, 1 );
-    this.container.appendChild( renderer.domElement );
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(store.bg.width, store.bg.height);
+    this.renderer.setClearColor(0x000000, 1);
+    this.container.appendChild(this.renderer.domElement);
   }
 
   render() {
     return (
-      <div ref={container => this.container = container} />
+      <div
+        ref={container => this.container = container}
+        data-store={JSON.stringify(this.props.store)}
+      />
     );
   }
 }
