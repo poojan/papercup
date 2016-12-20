44408277cd0b5e67119166ff905dbe78f290cef4
Poojan Shrestha <poojans@gmail.com>
2016-12-20 07:51:43 +0545

    Prepares espresso pass 1.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import './index.css';
 // import data from './data/8oz';
 // import data from './data/woodenTableCloseup';
 // import data from './data/handSmartPhone';
-import data from './data/rayWhite';
-// import data from './data/preparesEspresso';
+// import data from './data/rayWhite';
+import data from './data/preparesEspresso';
 
 import CupStore from './stores/CupStore';
 const cupStore = new CupStore(data);
