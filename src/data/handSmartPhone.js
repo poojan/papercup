d1d3b7f131c909699a64c33304d1c953a56a85d8
Poojan Shrestha <poojans@gmail.com>
2016-12-20 07:28:57 +0545

    handSmartPhone pass 1.

--- src/data/handSmartPhone.js
+++ src/data/handSmartPhone.js
@@ -1,12 +1,12 @@
 const bgImage = 'img/bg/stock-photo-take-away-paper-cup-with-the-hand-of-man-play-smart-phone-456359746.jpg';
 const cupImage = 'img/user/cuplogo.png';
 const overlayImage = null;
-const scaling = 0.35;
+const scaling = 1;
 
 export default {
   scene: {
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 800,
+    height: 600,
   },
 
   bg: {
@@ -31,23 +31,23 @@ export default {
   },
 
   camera: {
-    posZ: 1000,
+    posZ: 1800,
     projection: 'perspective',
-    fov: 25,
+    fov: 34,
     // projection: 'orthographic',
   },
 
   cup: {
-    "radiusTop": 79.4,
-    "radiusBottom": 66.60000000000001,
-    "height": 148.4,
-    "posX": 47.2,
-    "posY": -17.1,
-    "posZ": -7,
-    "rotX": 14.77,
+    "radiusTop": 225.10000000000002,
+    "radiusBottom": 198.60000000000002,
+    "height": 427.90000000000003,
+    "posX": 133.1,
+    "posY": -36.4,
+    "posZ": 0,
+    "rotX": 18.86,
     "rotY": 0,
-    "rotZ": 1,
-    "opacity": 0.8,
+    "rotZ": 1.1,
+    "opacity": 1,
     "openEnded": 1,
     image: cupImage,
   },
