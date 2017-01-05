036d12e2a7193c41b152e9ff53e0360d1484a0a7
Poojan Shrestha <poojans@gmail.com>
2017-01-05 17:33:27 +0545

    Tiled images into a grid for mockups image selection.

--- src/components/EmailMockups.js
+++ src/components/EmailMockups.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+import { action } from 'mobx';
+import Cup from './Cup';
 
+@inject('cupStore')
+@observer
 export default class EmailMockups extends Component {
+  @action onClickCup(keyId) {
+    console.log('keyId', keyId);
+  }
+
   render() {
+    const { cupStore } = this.props;
+
     return (
-      <h1>EmailMockups</h1>
+      <div className="Grid">
+        {cupStore.items.map(item => (
+          <Cup
+            key={item.id}
+            containerId={item.id}
+            keyId={item.id}
+            width={200 * 1.5}
+            height={150 * 1.5}
+            onClickCup={this.onClickCup}
+          />
+        ))}
+      </div>
     );
   }
 }
